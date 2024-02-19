c7e0b8184698ef43aa3ad5aa63279afdc7cf961e
Hamza khan <93365597+HamzaKhan37@users.noreply.github.com>
2024-02-19 12:29:13 +0530

    Add files via upload

---
+++ your_ml_model.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+import joblib
+import scipy
+import numpy as np
+import warnings
+
+def predict_sentiment(input_data):
+    df=pd.read_csv(r"C:\Users\Hamza\Downloads\Simsy-data.csv",encoding="latin1")
+    df=df[["BMC step type","Problem confident","Problem current progress","Problem ideas","Problem level support","Problem rate","Creator"]]
+    df1=df.dropna()
+    df1.drop("Problem ideas",axis=1,inplace=True)
+    # input_data = {
+    # 'BMC step type': ["REVENUE STREAMS"],
+    # 'Problem confident': ["Fair"],
+    # 'Problem current progress': ["In progress - 20%"],
+    # 'Problem level support':["Full support required	"],
+    # 'Problem rate': [2],  
+    # 'Creator': ["hjghjkjdff@gmail.com"]
+    # }
+    input_df=pd.DataFrame(input_data)
+    combined_df = pd.concat([df1, input_df], ignore_index=True)
+    cat_columns = ['BMC step type', 'Problem confident', 'Problem current progress', 'Problem level support']
+    one_hot_encoded = pd.get_dummies(combined_df[cat_columns])
+    combined_data = pd.concat([one_hot_encoded, combined_df[['Problem rate']]], axis=1)
+    scaler = StandardScaler()
+    numerical_data = scaler.fit_transform(combined_data)
+    warnings.filterwarnings(action='ignore')
+    wcss = []
+    scaled_data=numerical_data
+    for i in range(1, 11):  
+        kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=42)
+        kmeans.fit(scaled_data)
+        wcss.append(kmeans.inertia_)
+    # Load the model using joblib
+    num_clusters = 10  
+    kmeans = KMeans(n_clusters=num_clusters, random_state=42)
+    cluster_labels = kmeans.fit_predict(numerical_data[:len(numerical_data)-1])
+    loaded_model = joblib.load(r'C:\Users\Hamza\Downloads\KMeans.pkl')
+    input_cluster = cluster_labels[-1]
+    similar_indices = [i for i, label in enumerate(cluster_labels) if label == input_cluster]
+    similarities = cosine_similarity(numerical_data[-1].reshape(1, -1), numerical_data[similar_indices])
+    most_similar_index = similar_indices[similarities.argmax()]
+
+    most_similar_row = df1.iloc[most_similar_index]
+
+    print("Most similar row:")
+    print(most_similar_row)
+    return most_similar_row
+predict_sentiment({
+    'BMC step type': ["EXISTING ALTERNATIVES"],
+    'Problem confident': ["Fair"],
+    'Problem current progress': ["In progress - 20%"],
+    'Problem level support':["Full support required	"],
+    'Problem rate': [2],  
+    'Creator': ["hjghjkjdff@gmail.com"]
+    })    
